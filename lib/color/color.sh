73f5bc159561f84262a5071bbbd3ab6099db3401
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-10 14:08:16 +0100

    implement color

---
+++ lib/color/color.sh
@@ -0,0 +1,11 @@
+# -- declare costant
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+LGREEN='\033[1;92m'
+CYAN='\033[0;36m'
+LGRAY='\033[0;37m'
+LCYAN='\033[0;96m'
+BLUE='\033[0;34m'
+WHITE='\033[1;97m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
