b3c7cc02b59239b8f5bee5073c90ba136bb3edfe
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-10 20:30:45 +0100

    create config.sh

---
+++ config.sh
@@ -0,0 +1,21 @@
+function config {
+	#clear
+	printf "$LCYAN
+    ____ ____ _  _ ____ _ ____
+    |    |  | |\ | |___ | | __
+    |___ |__| | \| |    | |__]
+    $NC\n"
+	printf ">>>>>>> $LGREEN${USRNAME}$NC\n"
+	printf "${LCYAN}[choice]${NC}@ginc-hub${LCYAN}>${NC} "
+	printf "${RED}git config --list --show-origin${NC}\n"
+	git config --list --show-origin
+	printf "\n ${RED}[${NC}0${RED}]${NC} exit\n"
+	printf " ${RED}[${NC}1${RED}]${NC} ${NC}config username\n"
+	printf " ${RED}[${NC}2${RED}]${NC} ${NC}config email\n"
+	printf " ${RED}[${NC}3${RED}]${NC} config credential helper\n"
+	printf " ${RED}[${NC}4${RED}]${NC} config all(username, email, credential)\n"
+	printf " ${RED}[${NC}5${RED}]${NC} logout\n\n"
+	printf "${LCYAN}[choice]${NC}@ginc-hub${LCYAN}>${NC} "
+	read -p "" local n
+}
+
