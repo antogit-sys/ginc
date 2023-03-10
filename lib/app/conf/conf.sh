c92a0fee72a6120b5b451f07f1d6935ce4ed5b56
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-10 23:18:19 +0100

    function conf

---
+++ lib/app/conf/conf.sh
@@ -0,0 +1,37 @@
+#
+# -- Function 
+# 
+
+function configure_username {
+	#clear
+	local name
+	#printf $NC
+	printf "\n${GREEN} - config username - ${NC}\n"
+	printf "\n${LCYAN}name: ${NC}" 
+	read name
+	git config user.name $name 	
+}
+
+function configure_email {
+	#clear
+	local email
+	#printf $NC
+	printf "\n${GREEN} - config email - ${NC}\n"
+	printf "\n${LCYAN}email: ${NC}" 
+	read email
+	git config user.email $email 	
+}
+
+function configure_credentialHelper {
+	local ch
+	printf "\n${GREEN} - config credential helper - ${NC}\n"
+	printf "\n${LCYAN}credential helper: ${NC}" 
+	read ch
+	git config credential.helper $ch	
+}
+
+function logout {
+	git config user.name ""
+	git config user.email ""
+	git config credential.helper ""
+}
