37ea1e2d938d8f78292e092f51fdfcbeef84bae7
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-10 14:42:54 +0100

    implement date user

--- ginc.sh
+++ ginc.sh
@@ -3,9 +3,9 @@
 source "lib/color/color.sh"
 
 logo(){
-	clear
+	#clear
 	printf "$LGREEN
-	██████╗ ██╗███╗   ██╗ ██████╗
+	 ██████╗ ██╗███╗   ██╗ ██████╗
 	██╔════╝ ██║████╗  ██║██╔════╝
 	██║  ███╗██║██╔██╗ ██║██║
 	██║   ██║██║██║╚██╗██║██║
@@ -14,17 +14,18 @@ logo(){
 	$NC\n"
 }
 
-function main {
-	logo	
+function main {	
 	local let c=1
 	while [[ $c != 0 ]]
 	do
 		logo
+		printf "\t${GREEN}username:${NC} $(git config user.name)\t\n"
+		printf "\t${GREEN}email:${NC} $(git config user.email)\n\n"
 		printf "\t${RED}[${NC}1${RED}]${NC} config\n"
 		printf "\t${RED}[${NC}2${RED}]${NC} download remote repository\n"
 		printf "\t${RED}[${NC}3${RED}]${NC} exit\n"
 		echo
-		printf "${LCYAN}#choice#> ${NC}"
+		printf "${LCYAN}[choice]${NC}@ginc-hub${LCYAN}>${NC} "
 		read -p "" c
 	done
 	
