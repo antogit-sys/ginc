222940dbf65539021803675a2a563eec3530ff16
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-10 20:31:06 +0100

    bug if corrected

--- ginc.sh
+++ ginc.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 source "lib/color/color.sh"
+source "config.sh"
+
+USRNAME=$(git config user.name)
+USREMAIL=$(git config user.email)
 
 logo(){
 	#clear
@@ -15,18 +19,28 @@ logo(){
 }
 
 function main {	
-	local let c=1
-	while [[ $c != 0 ]]
+	local c=""
+	while [[ $c != "0" ]]
 	do
 		logo
-		printf "\t${GREEN}username:${NC} $(git config user.name)\t\n"
-		printf "\t${GREEN}email:${NC} $(git config user.email)\n\n"
+		printf "\t${GREEN}username:${NC} ${USRNAME}\t\n"
+		printf "\t${GREEN}email:${NC} ${USREMAIL}\n\n"
 		printf "\t${RED}[${NC}1${RED}]${NC} config\n"
 		printf "\t${RED}[${NC}2${RED}]${NC} download remote repository\n"
 		printf "\t${RED}[${NC}3${RED}]${NC} exit\n"
 		echo
 		printf "${LCYAN}[choice]${NC}@ginc-hub${LCYAN}>${NC} "
-		read -p "" c
+		read c
+		if [[ $c == "1" ]] 
+		then
+			config
+		elif [[ $c == "2" ]]
+		then
+			echo
+		elif [[ $c == "3" ]]
+		then
+			break
+		fi
 	done
 	
 }
